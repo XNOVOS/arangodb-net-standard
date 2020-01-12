635840b3c05c5ba218fb33a33f16599d55d4277f
Adam Pawsey <adam.pawsey@xnovos.com>
2020-01-12 22:46:08 +0200

    Fix test settings file

--- arangodb-net-standard.Test/ArangoDbTestSettings.cs
+++ arangodb-net-standard.Test/ArangoDbTestSettings.cs
@@ -2,9 +2,9 @@
 {
     public static class ArangoDBTestSettings
     {
-        public const string ArangoHost = "localhost.fiddler";
+        public const string ArangoHost = "localhost";
         public const string RootUsername = "root";
-        public const string RootPassword = "***REMOVED***";
+        public const string RootPassword = "root";
         public const string TestUsername = "abcxyz";
         public const string TestPassword = "abczyx";
         public const bool EnableJsonSerializationTracing = true;
